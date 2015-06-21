4227b1db5fb585fbc1ca2e9701ef32912f3cf53e
Drew Chandler <drew@Dizzles-MacBook-Pro.local>
2015-06-20 21:20:58 -0400

    npm init, added a bunch of shit for gulp and did an arms race

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+#!/usr/bin/nodejs
+
+/* global _: true */
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    bourbonPaths = require('node-bourbon').includePaths,
+    livereload = require('gulp-livereload'),
+    connect = require('gulp-connect'),
+    $ = require('gulp-load-plugins')();
+
+
+
+// ----------------------------------------
+// ----------------------------------------
+// ----------------------------------------
+// ------------ GULP CONNECT --------------
+// ----------------------------------------
+// ----------------------------------------
+// ----------------------------------------
+gulp.task('connect', function() {
+  connect.server();
+});
+
+
+
+// ----------------------------------------
+// ----------------------------------------
+// ----------------------------------------
+// ------------ GULP SASSY ----------------
+// ----------------------------------------
+// ----------------------------------------
+// ----------------------------------------
+gulp.task('sassy', function () {
+    return gulp.src(__dirname + '/build/sass/style.scss')
+    .pipe($.plumber({ errorHandler: $.notify.onError("SASS compile has failed! <%= error %>") }))
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+        errLogtoConsole: true,
+        includePaths: bourbonPaths
+    }))
+    .pipe($.sourcemaps.write('./'))
+    .pipe(gulp.dest(__dirname + '/assets/css'))
+    .pipe($.notify({'title': 'SASScess', 'message': 'SASS build has succeeded!'}));
+});
+
+
+
+
+
+
+
+// watch
+gulp.task('watch', function () {
+    livereload.listen();
+    gulp.watch(['build/sass/**'], ['sassy']).on('change', livereload.changed);
+    gulp.watch(['build/js/*.js']).on('change', livereload.changed);
+    gulp.watch(['*.html']).on('change', livereload.changed);
+    gulp.watch(['build/css/**']).on('change', livereload.changed);
+});
+
+// default
+gulp.task('default', [
+    'connect',
+    'sassy',
+    'watch'
+]);
